5b503521b1b86210113db714c97daf447d80c150
Pavel Klyuev <71312774+PavelKlyuev@users.noreply.github.com>
2023-04-13 10:24:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mannwhitneyu
+from scipy.stats import ttest_ind
 
 chat_id = 240671999 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    stat, pval = mannwhitneyu(x, y, alternative='two-sided')
+    stat, pval = ttest_ind(x, y, equal_var=False)
     return False if pval > 0.08 else True
